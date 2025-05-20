2f3d74f981ae67bbe12d7acc48b21395d50e9836
jgdiaz19 <jgdiaz19@hotmail.com>
2025-05-20 17:57:14 -0600

    add class nodo for queuque exercise

---
+++ src/main/java/uv/estructuras_datos/_1_primitivas/_3_colas/E0_Cola_Impresion/Nodo.java
@@ -0,0 +1,12 @@
+package uv.estructuras_datos._1_primitivas._3_colas.E0_Cola_Impresion;
+
+public class Nodo {
+    public Documento documento;
+    public Nodo siguiente;
+
+    public Nodo(Documento documento) {
+        this.documento = documento;
+        this.siguiente = null;
+    }
+}
+
